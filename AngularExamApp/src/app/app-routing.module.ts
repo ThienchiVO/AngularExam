bb120f854ad4b334041392896ea8f576d1e9e2f1
Thien chi VO <thienchi.vo.perso@gmail.com>
2023-05-10 12:30:54 +0200

    Mise en place de ds langues en(locale), fr et es

--- AngularExamApp/src/app/app-routing.module.ts
+++ AngularExamApp/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {BuildingPageComponent} from "./building-page/building-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '*', component: BuildingPageComponent },
+  { path: '', component: BuildingPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- AngularExamApp/src/app/app.module.ts
+++ AngularExamApp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BuildingPageComponent } from './building-page/building-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BuildingPageComponent
   ],
   imports: [
     BrowserModule,
